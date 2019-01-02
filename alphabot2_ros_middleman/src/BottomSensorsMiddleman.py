169717b1971fcc5bfd75d51f12b825d4ad5f15dd
Diogo Duque <diogoduque13@hotmail.com>
2019-01-02 18:46:09 +0000

    bottom sensors middleman is halfway through

---
+++ alphabot2_ros_middleman/src/BottomSensorsMiddleman.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+
+# ROS imports
+import rospy
+import message_filters
+from std_msgs.msg import Int32MultiArray
+from sensor_msgs.msg import LaserScan, Image
+from geometry_msgs.msg import Twist
+import math
+from cv_bridge import CvBridge, CvBridgeError
+
+algorithmTopic = None
+
+def callback(sensor1, sensor2, sensor3, sensor4, sensor5):
+    global algorithmTopic
+
+    msg = Int32MultiArray()
+    print CvBridge().imgmsg_to_cv2(sensor1, "bgr8")
+    print len(sensor1.data)
+
+    #algorithmTopic.publish(msg)
+
+
+def main():
+    global algorithmTopic
+
+    rospy.init_node('alphabot2_bottom_sensors_middleman')
+
+    algorithmTopic = rospy.Publisher('/alphabot2/bottom_sensors', Int32MultiArray, queue_size=1)
+
+    sub1 = message_filters.Subscriber('/sensor1_bottom/image_raw', Image)
+    sub2 = message_filters.Subscriber('/sensor2_bottom/image_raw', Image)
+    sub3 = message_filters.Subscriber('/sensor3_bottom/image_raw', Image)
+    sub4 = message_filters.Subscriber('/sensor4_bottom/image_raw', Image)
+    sub5 = message_filters.Subscriber('/sensor5_bottom/image_raw', Image)
+
+    ts = message_filters.TimeSynchronizer([sub1, sub2, sub3, sub4, sub5], 10)
+    ts.registerCallback(callback)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
