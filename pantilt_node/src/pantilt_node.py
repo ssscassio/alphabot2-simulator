a94cdaa483711981bd4ed6877592acc5ebd89fc9
Cássio Santos <sss.cassio@gmail.com>
2019-01-03 19:31:36 +0000

    Fix pan tilt node

--- pantilt_node/src/pantilt_node.py
+++ pantilt_node/src/pantilt_node.py
@@ -1,61 +1,89 @@
 #!/usr/bin/env python
 
-import rospy
-from std_msgs.msg import String
-from std_msgs.msg import Float64MultiArray
-from std_msgs.msg import Float64
-import math
-import RPi.GPIO as GPIO
-import time
+# import rospy
+# from std_msgs.msg import Float64
+# from utils.PCA9685 import PCA9685
+# import math
+# import RPi.GPIO as GPIO
+# import time
 
-servoPIN = 17
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN, GPIO.OUT)
-
-#Subscribe to:
-lower_angle = std_msgs/Float64 #pan - angle in degrees
-upper_angle = std_msgs/Float64 #tilt - angle in degrees
-
-#Publish to:
-int in_min = -90
-int in_max = +90
-int MIN_PULSE_WIDTH = 650
-int MAX_PULSE_WIDTH = 2350
+IN_MIN = -90
+IN_MAX = +90
+MIN_PULSE_WIDTH = 650
+MAX_PULSE_WIDTH = 2350
+DEFAULT_PULSE_WIDTH = 1500
+FREQUENCY = 50
 
 
-def get_angle():
+class alphabot_pantilt:
+  def __init__(self):
+    self.pwm = PCA9685(0x40, debug=False) 
+    self.pwm.setPWMFreq(50) # PWM with 50Hz
 
-def degree_to_rad_lower():
-    angle_rad = angle_lower*math.pi/180
+  def get_pulse_width(angle_degree):
+    global IN_MIN, IN_MAX, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH, DEFAULT_PULSE_WIDTH, FREQUENCY
+
+    pulse_wide = (angle_degree - IN_MIN)*(MAX_PULSE_WIDTH-MIN_PULSE_WIDTH)/(IN_MAX-IN_MIN)+MIN_PULSE_WIDTH
+    get_pulse_width = int(float(pulse_wide) / 1000000 * FREQUENCY * 4096)
+    return get_pulse_width
+
+  def move (self, channel, angle_degree) :
+    self.pwm.setServoPulse(channel, self.get_pulse_width(angle_degree))
+
+class pan_tilt_node:
+  def __init__(self):
+
+    """ Subscribe to alphabot vertical and horizontal camera controller (Angles in degrees)"""
+    self.vertical_sub = rospy.Subscriber("/alphabot2_vertical", Float64, self.vertical_callback)
+    self.horizontal_sub = rospy.Subscriber("/alphabot2_horizontal", Float64, self.horizontal_callback)
+
+    """ Create Gazebo Publishers """
+    self.gazebo_vertical_pub = rospy.Publisher("/alphabot2/joint_upper_camera_position_controller/command", Float64, queue_size=10)
+    self.gazebo_horizontal_pub = rospy.Publisher("/alphabot2/joint_lower_camera_position_controller/command", Float64, queue_size=10)
+
+    self.real_robot =  alphabot_pantilt()
+
+  def vertical_callback(angle_degree):
+    angle_rad = self.convert_degree_to_rad(angle_degree)
     
-    pub_lower_angle = rospy.Publisher("/alphabot2/joint_lower_camera_position_controller/command", Float64, queue_size=10)
-    rospy.init_node('pub_lower_angle', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        rospy.loginfo(pub_lower_angle)
-        pub.publish(pub_lower_angle)
-        rate.sleep()
+    """ Publishing to Gazebo """
+    self.gazebo_vertical_pub.publish(angle_rad)
 
-def analog_value(angle):
-    pulse_wide = (angle - in_min)*(MAX_PULSE_WIDTH-MIN_PULSE_WIDTH)/(in_max-in_min)+MIN_PULSE_WIDTH
-    analog_value = int(float(pulse_wide) / 1000000 * FREQUENCY * 4096)
-    return analog_value
+    """ Publishing to Alphabot2 """
+    self.real_robot.move(0, angle_degree)
 
-def degree_to_dutyCycle():
-    duttyCycle_lower= analog_value(lower_angle)
-    duttyCycle_upper= analog_value(upper_angle)
-    p.ChangeDutyCycle(duttyCycle_lower)
-    p.ChangeDutyCycle(duttyCycle_upper)
-    time.sleep(0.5)
+  def horizontal_callback(angle_degree):
+    angle_rad = self.convert_degree_to_rad(angle_degree)
+    
+    """ Publishing to Gazebo """
+    self.gazebo_horizontal_pub.publish(angle_rad)
+
+    """ Publishing to Alphabot2 """
+    self.real_robot.move(1, angle_degree)
+
+  def convert_degree_to_rad(angle_degree):
+    return angle_degree*math.pi/180
+
+
+# def degree_to_rad_lower():
+    # angle_rad = angle_lower*math.pi/180
+    
+    # pub_lower_angle = rospy.Publisher("/alphabot2/joint_lower_camera_position_controller/command", Float64, queue_size=10)
+    # rospy.init_node('pub_lower_angle', anonymous=True)
+    # rate = rospy.Rate(10) # 10hz
+    # while not rospy.is_shutdown():
+    #     rospy.loginfo(pub_lower_angle)
+    #     pub.publish(pub_lower_angle)
+    #     rate.sleep()
+
+
+def main():
+  ptn = pan_tilt_node()
+  rospy.spin()
 
 
 if __name__ == '__main__':
-    p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-    p.start(2.5) # Initialization
-    get_angle() #Subscribe
-     try:
-         degree_to_rad_lower() #Publish to simulator
-         degree_to_rad_upper() #Publish to simulator
-         degree_to_dutyCycle() #Publish to alphabot2
-     except rospy.ROSInterruptException:
-         pass
+  try:
+    main()
+  except rospy.ROSInterruptException:
+    pass
